fef9fa78372b2dd7cb5d88d6265931bc6bb58c1e
bayuwira <bayuwira1718@gmail.com>
2022-03-13 16:35:48 +0800

    first commit

---
+++ Test Suites/Master.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Master</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>bayuwira1718@gmail.com;</mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>285fc83e-5f31-4557-90d5-c3eec0008eef</testSuiteGuid>
+   <testCaseLink>
+      <guid>ccea4a9f-7043-436c-86eb-deafe3dcc2e3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Users/GET User Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4444593-dfd4-47c2-927e-c0fd971cb514</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Users/DELETE User Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e17ea936-825d-4a78-b836-76a915b2defb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Users/CREATE Users Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>81cdb317-0c94-4619-85c4-aa84f62aa18a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Users/UPDATE User Test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
